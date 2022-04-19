fbe9e7206117140bdc0347b18d8c7b38f18fbe33
abdolrhman <iwcw4i@gmail.com>
2022-04-19 16:25:10 +0200

    integrate the josn file with mongodb

--- db/db.go
+++ db/db.go
@@ -2,11 +2,17 @@ package db
 
 import (
 	"context"
+	"encoding/json"
+	"fmt"
 	"github.com/fatih/color"
 	middlewares "github.com/umangraval/Go-Mongodb-REST-boilerplate/handlers"
+	"github.com/umangraval/Go-Mongodb-REST-boilerplate/models"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
+	"io/ioutil"
 	"log"
+	"path/filepath"
+	"reflect"
 )
 
 var client *mongo.Client
@@ -27,5 +33,46 @@ func Dbconnect() *mongo.Client {
 	}
 	color.Green("⛁ Connected to Database")
 
+	loadDataFromJsonFile(err, client)
+
 	return client
+}
+
+func loadDataFromJsonFile(err error, client *mongo.Client) {
+	// Load values from JSON file to model
+	docsPath, _ := filepath.Abs("db/db.json")
+
+	byteValues, err := ioutil.ReadFile(docsPath)
+	if err != nil {
+		// Print any IO errors with the .json file
+		fmt.Println("ioutil.ReadFile ERROR:", err)
+	} else {
+		// Access a MongoDB collection through a database
+		col := client.Database("wajve").Collection("trivia")
+		fmt.Println("Collection type:", reflect.TypeOf(col), "n")
+
+		// Print the values of the JSON docs, and insert them if no error
+		fmt.Println("ioutil.ReadFile byteValues TYPE:", reflect.TypeOf(byteValues))
+		fmt.Println("byteValues:", byteValues, "n")
+		fmt.Println("byteValues:", string(byteValues))
+		var docs []models.Trivia
+		// Unmarshal the encoded JSON byte string into the slice
+		err = json.Unmarshal(byteValues, &docs)
+		fmt.Println("nMongoFields Docs:", reflect.TypeOf(docs))
+		for i := range docs {
+			// Put the document element in a new variable
+			doc := docs[i]
+			fmt.Println("ndoc _id:", doc.Text)
+			fmt.Println("doc Field Str:", doc.Text)
+
+			// Call the InsertOne() method and pass the context and doc objects
+			result, insertErr := col.InsertOne(context.TODO(), doc)
+			// Check for any insertion errors
+			if insertErr != nil {
+				fmt.Println("InsertOne ERROR:", insertErr)
+			} else {
+				fmt.Println("InsertOne() API result:", result)
+			}
+		}
+	}
 }
